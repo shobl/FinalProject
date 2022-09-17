a555d3dbd7358ec74ed96d73fb5e363811222840
Vika <karpenkoviktori25@gmail.com>
2022-09-17 11:55:38 +0300

    entered forecast

--- index.js
+++ index.js
@@ -23,37 +23,46 @@ function formatDate(date) {
   return `${day} ${hours}:${minutes}`;
 }
 
-/*function displayForecast(response) {
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+
+}
+
+function displayForecast(response) {
   let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
+  
   let forecastHTML = `<div class="row">`;
   forecast.forEach(function(forecastDay, index) {
     if (index < 6) {
       forecastHTML = forecastHTML + `
-       
-        <div class="col-2">
-          <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+    <div class="col-2">
+      <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
           <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt="" width="42" />
-          <div class="weather-forecast-temperatures">
+      <div class="weather-forecast-temperatures">
             <span class="weather-forecast-temperature-max">
             ${Math.round(forecastDay.temp.max)}°
             </span>
             <span class="weather-forecast-temperature-min">
             ${Math.round(forecastDay.temp.min)}°
             </span>
-          </div>
-        </div>
+          
+        
       </div>
       
     </div>
 
     `;
     }
-  })
+  });
   forecastHTML = forecastHTML + `</div>`
   forecastElement.innerHTML = forecastHTML;
 }
-*/
 
 function search(event) {
   event.preventDefault();
@@ -70,7 +79,7 @@ function getForecast(coordinates) {
 }
    
 function displayWeatherCondition(response) {
-  console.log(response.data);
+   
   document.querySelector("#city").innerHTML = response.data.name;
 
   celsiusTemperature = response.data.main.temp
@@ -83,7 +92,7 @@ function displayWeatherCondition(response) {
   iconElement.setAttribute("src", ` http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
   iconElement.setAttribute("alt", response.data.weather[0].description);
 
-  /*getForecast(response.data.coord);*/
+  getForecast(response.data.coord);
 }
 
 function searchCity(city) {
